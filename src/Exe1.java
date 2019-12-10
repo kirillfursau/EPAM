bec21e7bc4c7a969deeb0d8b6fe1db07298765ff
kirillfursau <krill19988@gmail.com>
2019-12-10 17:01:55 +0300

    Nothing

--- src/Exe1.java
+++ src/Exe1.java
@@ -1,2 +1,9 @@
 public class Exe1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int z = ((a - 3) * b / 2) + c;
+        System.out.println(z);
+    }
 }
---
+++ src/Exe2.java
@@ -0,0 +1,8 @@
+public class Exe2 {
+    public static void main(String[] args) {
+        int b =  6;
+        int a = 3;
+        int c =0;
+        c = (b + )
+    }
+}
