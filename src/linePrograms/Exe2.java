c101e522356045dc4c8d8a842c56d8e6e9b3568b
kirillfursau <krill19988@gmail.com>
2020-02-03 17:53:44 +0300

    Done

--- src/linePrograms/Exe2.java
+++ src/linePrograms/Exe2.java
@@ -5,7 +5,7 @@ public class Exe2 {
         int b = 6;
         int a = 3;
         int c = 0;
-        double d = ((b + Math.sqrt((Math.pow(b, 2)) + (4 * a * c))) / (2 * a)) - ((Math.pow(a, 3)) * c) + Math.pow(b, 2);
-        ;
+        double d = ((b + Math.sqrt((Math.pow(b, 2)) + (4 * a * c))) / (2 * a)) - ((Math.pow(a, 3)) * c)
+                + Math.pow(b, -2);
     }
 }
