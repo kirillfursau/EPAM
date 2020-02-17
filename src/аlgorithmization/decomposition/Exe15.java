235bf6fe124619d3d0cd5480d13a5b8c20ca0af8
kirillfursau <krill19988@gmail.com>
2020-02-17 21:32:39 +0300

    Done

--- src/аlgorithmization/decomposition/Exe15.java
+++ src/аlgorithmization/decomposition/Exe15.java
@@ -8,29 +8,29 @@ public class Exe15 {
         */
         int n = 5;
         int[] array = new int[n];
-        int N = (int) Math.pow(10, n) - 1;
-        for (int i = 0; i < N; i++) {
+        int number = (int) Math.pow(10, n) - 1;
+        for (int i = 0; i < number; i++) {
             numberInArray(i, array);
-            if (isIncreasing(array) == 1) {
+            if (isIncreasing(array) == true) {
                 System.out.println(i);
             }
         }
     }
 
-    private static void numberInArray(int number, int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = number % 10;
+    private static void numberInArray(int number, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = number % 10;
             number /= 10;
         }
     }
 
 
-    private static int isIncreasing(int[] array) {
+    private static boolean isIncreasing(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] <= array[i + 1]) {
-                return 0;
+                return false;
             }
         }
-        return 1;
+        return true;
     }
 }
